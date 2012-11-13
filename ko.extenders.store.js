df9f479f83748e1ef068a9a03491ea9c1a5d0de8
rjkip <github@reinierkip.nl>
2012-11-13 09:41:25 +0100

    Converting to Jam package.

--- ko.extenders.store.js
+++ ko.extenders.store.js
@@ -1,10 +1,14 @@
-(function() {
+define(
+    "ko.extenders.store",
+    ["knockout", "store"],
+    function (ko, store) {
 
-  ko.extenders.store = function(observable, key_name) {
-    observable(store.get(key_name));
-    observable.subscribe(function(new_value) {
-      store.set(key_name, new_value);
-    });
-  };
+        ko.extenders.store = function (observable, key_name) {
+            observable(store.get(key_name));
+            observable.subscribe(function (new_value) {
+                store.set(key_name, new_value);
+            });
+        };
 
-}).call(this);
+    }
+);
